a58ab65e4d0e1d1af37c1437fbdf62e4d05b0737
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-11-20 17:05:06 -0300

    alterando yml

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     
-      baseUrl: 'http://lojaebac.ebaconline.art.br/',
+   baseUrl: 'http://lojaebac.ebaconline.art.br/',
    reporter: "mochawesome",
    reporterOptions: {
     reportDir: "mochawesome-report",
