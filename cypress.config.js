4e8ee01ae7f50e15767b21d0ef74de5186111c76
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-11-20 16:36:54 -0300

    salvando gitactions

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     
-      baseUrl: 'http://lojaebac.ebaconline.art.br/'
-   
+      baseUrl: 'http://lojaebac.ebaconline.art.br/',
+   reporter: "mochawesome",
+   reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    reportFilename: "index.html",
+    html: true,
+    json: false
+   }
   }
 });
